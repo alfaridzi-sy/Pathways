ca591948b7b69fd4187620986c26678dfeced1db
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2022-07-27 15:04:23 +0700

    Initial Commit

---
+++ Pathways/PathwaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathwaysApp.swift
+//  Pathways
+//
+//  Created by Syafitra Alfa Ridzi on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PathwaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
